31c98c30ec6d252f0582b7e851dbfcdfc31134b8
maxiamikel19 <maxloversist19@gmail.com>
2026-04-04 17:17:40 -0300

    feat: crear el proyecto

---
+++ src/main/java/com/github/maxiamikel/attendancemanagementapi/AttendancemanagementapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.maxiamikel.attendancemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendancemanagementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendancemanagementapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/maxiamikel/attendancemanagementapi/AttendancemanagementapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maxiamikel.attendancemanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendancemanagementapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
