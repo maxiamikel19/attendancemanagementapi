cc8f2e54585c0ea895aee0b77233c941b35b52f2
maxiamikel19 <maxloversist19@gmail.com>
2026-04-18 11:40:02 -0400

    feat: deploy en render

--- src/main/java/com/github/maxiamikel/attendancemanagementapi/config/OpenApiConfig.java
+++ src/main/java/com/github/maxiamikel/attendancemanagementapi/config/OpenApiConfig.java
@@ -1,7 +1,5 @@
 package com.github.maxiamikel.attendancemanagementapi.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +9,6 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
@@ -26,8 +23,6 @@ public class OpenApiConfig {
                                                 .contact(new Contact()
                                                                 .name("Amikel Maxi")
                                                                 .email("maxloversist@gmail.com")))
-                                .servers(List.of(
-                                                new Server().url("http://localhost:8080").description("Local")))
                                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                                 .components(new Components()
                                                 .addSecuritySchemes("bearerAuth",
